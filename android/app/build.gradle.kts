aec2e943bbb40b8f4c6272b71d5befc7b0d23bc6
dimmov21 <dimmov21@gmail.com>
2025-09-02 23:23:47 +0300

    change name

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.snake_game"
+    namespace = "com.simplegames.snakegame"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.snake_game"
+        applicationId = "com.simplegames.snakegame"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
